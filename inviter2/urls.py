c28a26593c1115673a12c23190e443d4cfd0af11
Michael J. Schultz <mjschultz@gmail.com>
2016-04-19 22:03:54 -0500

    Convert to new urlpatterns form

--- inviter2/urls.py
+++ inviter2/urls.py
@@ -1,11 +1,10 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import Register, Done, OptOut, OptOutDone
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^done/$', Done.as_view(), name='done'),
     url(r'^register/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]+)/$',
         Register.as_view(), name='register'),
@@ -13,7 +12,7 @@ urlpatterns = patterns(
         OptOut.as_view(), name='opt-out'),
     url(r'^optout/done/$',
         OptOutDone.as_view(), name='opt-out-done'),
-)
+]
 """
 .. attribute:: ^done/$
 
